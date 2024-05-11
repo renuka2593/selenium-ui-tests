776048476f6475ce2cf1631e22354e1a391115a8
sanjeev.kumar1 <renukasharma25sept@gmail.com>
2024-05-11 21:57:47 +0530

    fixes

--- src/test/java/com/renuka/test/BaseTest.java
+++ src/test/java/com/renuka/test/BaseTest.java
@@ -3,18 +3,23 @@ package com.renuka.test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 
 public class BaseTest {
     protected WebDriver driver;
 
-    @BeforeTest
+    @BeforeClass
     public void testInitialization() {
-        driver = new ChromeDriver();
+        ChromeOptions option = new ChromeOptions();
+        option.addArguments("headless");
+        driver = new ChromeDriver(option);
     }
 
-    @AfterTest
+    @AfterClass
     public void testCleanUp() {
         driver.quit();
     }
